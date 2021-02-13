920e37d374656dfed64ec17ecebdac6b98f69ca5
artpol2001 <artpol2001@gmail.com>
2021-02-13 14:09:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/anst_21/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.anst_21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
